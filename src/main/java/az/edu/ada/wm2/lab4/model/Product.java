be2e52bb2066b76d862264e0ccdf467d896da6e3
bmuradova17974 <Bmuradova17974@ada.edu.az>
2026-02-25 21:05:33 +0400

    Add ProductRepositoryImpl implementation

--- src/main/java/az/edu/ada/wm2/lab4/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab4/model/Product.java
@@ -56,6 +56,10 @@ public class Product {
         this.expirationDate = expirationDate;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
